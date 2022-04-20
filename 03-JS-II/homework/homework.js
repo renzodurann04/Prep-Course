f9c7435cf941c5a3c3a949f98ead46d96d6d37f1
Renzo <duranrenzo3@gmail.com>
2022-04-20 11:38:20 -0300

    una pavada agregué

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -182,6 +182,7 @@ function tablaDelSeis() {
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí   
+  //me llamo renzo jajajaja
   let arrayTablaDelSeis = [];
   for (let i = 0; i < 11; i++) {
     arrayTablaDelSeis.push(6 * i);
